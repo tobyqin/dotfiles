5b8a8c5a5f7761e3cd3e4bb102f65a5a1f697aea
Toby Qin <toby.qin@live.com>
2017-05-12 22:21:50 +0800

    add cookiecutter config file

--- windows.bat
+++ windows.bat
@@ -3,6 +3,8 @@ echo Restore dotfiles on Windows
 
 copy .gitconfig %userprofile% /y
 copy .npmrc %userprofile% /y
+copy .cookiecutterrc %userprofile% /y
+
 if not exist %userprofile%\pip mkdir %userprofile%\pip
 copy pip %userprofile%\pip /y
 if not exist %userprofile%\.pypirc copy .pypirc %userprofile%
